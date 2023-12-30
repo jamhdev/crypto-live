5b1bb1c6221d4b863f071d2609462914ab556959
Klexup <jameshensley37@gmail.com>
2023-12-30 05:40:02 -0700

    small svg width change and text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function LandingPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      Landing Page
+      Home
     </main>
   );
 }
