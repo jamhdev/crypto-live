ed25e75333f485d41d153dc5a403346f60f81105
Klexup <jameshensley37@gmail.com>
2024-01-10 02:04:35 -0700

    created MarketDataNav component with dynamic functionality

--- app/interfaces/ContextInterfaces.ts
+++ app/interfaces/ContextInterfaces.ts
@@ -15,3 +15,16 @@ interface CreateContextType {
   };
   currencyFormat: Intl.NumberFormat;
 }
+
+interface GlobalMarketDataType {
+  active_cryptocurrencies: number;
+  markets: number;
+  total_market_cap: {
+    usd: number;
+  };
+  market_cap_change_percentage_24h_usd: number;
+  market_cap_percentage: {
+    btc: number;
+    eth: number;
+  };
+}
