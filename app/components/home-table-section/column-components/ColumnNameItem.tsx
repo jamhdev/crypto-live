c0ca538f0bb25778ca66614b3a03dbb899e0945e
Klexup <jameshensley37@gmail.com>
2024-01-21 10:07:43 -0700

    updated img src

--- app/components/home-table-section/column-components/ColumnNameItem.tsx
+++ app/components/home-table-section/column-components/ColumnNameItem.tsx
@@ -12,7 +12,7 @@ export default function ColumnNameItem({
   return (
     <>
       <div className="flex items-center gap-4 p-4">
-        <img src={`${image}`} alt="Coin Image" width={32} height={32} />
+        <img src={image} alt="Coin Image" width={32} height={32} />
         {name.charAt(0).toUpperCase()}
         {name.slice(1)}({symbol.toUpperCase()})
       </div>
