ee3e2a2cc061c99cf4cdbcb956d35a5b5d847993
klexup <jameshensley37@gmail.com>
2024-05-29 18:34:10 -0600

    added loading and error states & styles for carousel

--- app/components/home-chart-and-carousel-section/HomeCarouselSection.tsx
+++ app/components/home-chart-and-carousel-section/HomeCarouselSection.tsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/app/store/store";
 import useEmblaCarousel from "embla-carousel-react";
 import { EmblaOptionsType } from "embla-carousel";
+import LoadingCircleLine from "@/public/LoadingCircleLineSvg.svg";
 export default function HomeCarouselSection({
   coinSelected,
   setCoinSelected,
@@ -34,6 +35,56 @@ export default function HomeCarouselSection({
   const scrollNext = useCallback(() => {
     if (emblaApi) emblaApi.scrollNext();
   }, [emblaApi]);
+
+  if (isLoading)
+    return (
+      <>
+        <div className="w-full relative">
+          <button className="embla__prev w-14 h-14 bg-highlightColor text-white rounded-full flex justify-center items-center transition-all absolute left-2 z-10 top-[10px] border-4 border-background hover:border-0">
+            &lt;
+          </button>
+          <button className="embla__next w-14 h-14 bg-highlightColor text-white rounded-full justify-center items-center transition-all absolute right-2 z-10 top-[10px] border-4 border-background hover:border-0">
+            &gt;
+          </button>
+          <div className="w-full flex gap-2 px-10 h-[78px] justify-center items-center">
+            <div className="w-[264px] h-[78px] border-highlightColor border-4 rounded-lg flex justify-center items-center">
+              <LoadingCircleLine />
+            </div>
+            <div className="w-[264px] h-[78px] border-highlightColor border-4 rounded-lg flex justify-center items-center">
+              <LoadingCircleLine />
+            </div>
+            <div className="w-[264px] h-[78px] border-highlightColor border-4 rounded-lg flex justify-center items-center">
+              <LoadingCircleLine />
+            </div>
+            <div className="w-[264px] h-[78px] border-highlightColor border-4 rounded-lg flex justify-center items-center">
+              <LoadingCircleLine />
+            </div>
+            <div className="w-[264px] h-[78px] border-highlightColor border-4 rounded-lg flex justify-center items-center">
+              <LoadingCircleLine />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+
+  if (error)
+    return (
+      <>
+        <div className="w-full relative">
+          <button className="embla__prev w-14 h-14 bg-highlightColor text-white rounded-full flex justify-center items-center transition-all absolute left-2 z-10 top-[10px] border-4 border-background hover:border-0">
+            &lt;
+          </button>
+          <button className="embla__next w-14 h-14 bg-highlightColor text-white rounded-full justify-center items-center transition-all absolute right-2 z-10 top-[10px] border-4 border-background hover:border-0">
+            &gt;
+          </button>
+          <div className="w-full flex gap-2 px-10 h-[78px] justify-center items-center">
+            <div className="w-full h-[78px] border-highlightColor border-4 rounded-lg flex justify-center items-center text-themeTextColor">
+              Error loading carousel
+            </div>
+          </div>
+        </div>
+      </>
+    );
 
   return (
     <>
